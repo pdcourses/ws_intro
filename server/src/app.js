5fedd984ce2d26ad9bdd04a15af323d7d356351d
julietvt <freshitpt@gmail.com>
2022-02-05 10:49:05 +0200

    ws hello in client

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,19 @@
+//WebSocket нужно написать и на сервере и на клиенте
+// WebSocket - протокол !!
+
 const express = require('express');
 const app = express();
+const expressWs = require('express-ws')(app);
+
+app.use(express.json());
+
+app.ws('/', function (ws, req) {
+  // метод on слушает сообщенние
+  ws.on('message', function (msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
