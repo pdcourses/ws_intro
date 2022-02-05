27b8252b83861282a1f3caf8c1bc5bc5b87e11ae
julietvt <freshitpt@gmail.com>
2022-02-05 12:16:37 +0200

    add array history

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,17 @@ const gWss = expressWs.getWss('/');
 
 app.use(express.json());
 
+const messages = [];
+
 app.ws('/', function (ws, req) {
+  ws.send(JSON.stringify(messages));
+
   ws.on('message', function (msg) {
     console.log(msg);
+    messages.push(msg);
+
     gWss.clients.forEach(function (client) {
-      client.send(msg);
+      client.send(JSON.stringify([msg]));
     });
   });
   console.log('socket', req.testing);
